42a1eed9774338a95b89a33e6830e1f058cba95f
Levi Rebouças Mota <126111083+levireboucasmota@users.noreply.github.com>
2024-09-30 09:48:03 -0300

    fix: Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://savemypressurizer-api.onrender.com"
+  baseURL: "https://savemypressurizer-api-ld9o.onrender.com"
 })
